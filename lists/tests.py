d878cd09bb625e7a138a0dc65d8423eec5cc240e
Xieyx121 <12781961+xieyx121@user.noreply.gitee.com>
2025-04-29 17:14:47 +0800

    16th commit: form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -24,14 +24,10 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
-class HomePageTest(TestCase):
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
+class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': "A new list item"})
+        response = self.client.post('/lists/new', data={'item_text': "A new list item"})
         
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -39,14 +35,16 @@ class HomePageTest(TestCase):
         
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-new-page/')
+        response = self.client.post('/lists/new', data={'item_text': "A new list item"})
+        self.assertRedirects(response, '/lists/the-new-page/')
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 
 
